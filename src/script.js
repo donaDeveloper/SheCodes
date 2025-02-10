8536823b7e971a58c50c6bf9ce048c494f023551
donaDeveloper <dona.developer25@gmail.com>
2025-02-10 01:47:46 -0300

    working

--- src/script.js
+++ src/script.js
@@ -1,38 +1,44 @@
-function refreshWeather(response){
-    let temperatureElement = document.querySelector("#Temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windSeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector("#icon");
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-cityElement.innerHTML = response.data.city;
-timeElement.innerHTML = formatDate(date);
-descriptionElement.innerHTML = response.data.condition.description;
-humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-windSeedElement.innerHTML = `${response.data.wind.speed}Km/h`;
-temperatureElement.innerHTML = Math.round(temperature);
-iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class = "weather-app-icon">`;
-
+  cityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSeedElement.innerHTML = `${response.data.wind.speed}Km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class = "weather-app-icon">`;
 }
 
-function formatDate(date){
-    let minutes = date.getMinutes();
-    let hours = date.getHours();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    
-    let day = days[date.getDay()];
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-    if(minutes < 10){
-        minutes = `0${minutes}`;
-    }
+  let day = days[date.getDay()];
 
-    return `${day} ${hours} ${minutes}`;
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day} ${hours} ${minutes}`;
 }
-
 
 /*function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
@@ -42,23 +48,22 @@ function formatDate(date){
   temperatureElement.innerHTML = temperature;
 }*/
 
-function searchCity(city){
+function searchCity(city) {
   /*event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
   let city = searchInputElement.value;*/
 
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "57971fb96f42c06235d5bbtba9e54c0o";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-
-  axios.get(apiUrl).then(refreshWeather);
+  https: axios.get(apiUrl).then(refreshWeather);
 }
 
-function handleSearchSubmit(event){
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
 
-    searchCity(searchInput.value);
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
